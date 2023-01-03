6f9fd23922457180cbc68e8053b11ec8c0fbbb82
Raul Mascarenhas <raulsmascarenhas@gmail.com>
2023-01-03 15:55:07 -0300

    Estudo atualizado

---
+++ CursoJS/comentarios/comentarios.js
@@ -0,0 +1,7 @@
+// Comentario de uma unica linha apos a "//", exemplo:
+console.log('Olá mundo!'); // comentario.
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ignorar tudo, serve para anotações.
---
+++ CursoJS/console_log/ex/ex_1.js
@@ -0,0 +1,11 @@
+// Ex 1 = console.log();
+
+console.log('Meu nome é "Raul". Estou aprendendo JavaScript às', 15 ,'da tarde.');
+
+// Testes
+
+console.log('Raul Mascarenhas'); // String = Texto
+console.log("Raul Mascarenhas");
+console.log(`Raul Mascarenhas`);
+
+console.log(123456, 15.85); // Number = Número
