61de8011ba7c03fbcda96dffb48f0dbf4d8a8dfe
Raul Mascarenhas <raulsmascarenhas@gmail.com>
2023-01-03 17:56:59 -0300

    Atualização dos estudos

---
+++ CursoJS/variaveis_teste/index.js
@@ -0,0 +1,23 @@
+let nome = 'João'; // String
+// "mesma" coisa que var
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+//teste 
+
+let exemplo; //Variavel declarada.
+exemplo = 'Qualquer valor'; // Inicializando variavel.
+console.log(exemplo); // mesmo não tendo inicializado, ele tem um valor (Undefined).
+
+//teste 2
+
+let nomes; // variavel declarada.
+nomes = 'Ana'; // inicializada.
+console.log(nomes); // primeiro parametro.
+nomes = 'Raul'; // variavel muda de valor a partir daqui.
+console.log(nomes); // segundo parametro.
+// não pode declarar a mesma variavel mais de uma vez.
---
+++ CursoJS/variaveis_teste/observacoes.js
@@ -0,0 +1,65 @@
+// Observação 1
+
+/* Não podemos criar variáveis com palavras reservadas.
+*  Exemplo: 
+   let let; let console; let if;
+   O sistema reconhece como erro.
+*/
+
+// Observação 2
+
+/* Variáveis devem ter nomes significativos e se possivel letras minúsculas.
+*  Comparações: 
+   sem significancia "let n = 'João'"
+   com significancia "let nome = 'João'"
+   Deixar o codigo mais significativo possivel.
+*/
+
+// Observação 3
+
+/* Não se pode começar o nome de uma variável com um número
+*  Exemplo:
+   let 1nome; let 1;
+   O sistema reconhece como erro.
+*/
+
+// Observação 4
+
+/* Não pode conter espaços ou traços.
+*  Exemplo:
+   let nome-Cliente; let nome Cliente; (Não funciona.)
+*  Se for necessario usar nomes compostos utilize camelCase:
+   let nomeCliente; let nomeCompleto;
+   Cada palavra apos a primeira deve iniciar em maiusculo.
+*  Exemplo:
+   let nomeCompletoDoCliente;
+*/
+
+// Observação 5
+
+/* Case-sensetive, palavras minusculas e maiusculas fazem diferença.
+*  Exemplo:
+   let nomeCliente = 'Luiz';
+   let nomecliente = 'Otávio'
+   São variaveis diferentes por conta do Case-sensitive, mas as duas funcionam.
+   Mesmo funcionando deve-se usar o camelCase.
+*/
+
+// Observação 6
+
+/* Não podemos redeclarar variaveis com let
+*  Exemplo:
+   let nomeCliente;
+   let nomeCliente; (não funciona)
+   Caso queira repetir parametros do mesmo let, apenas passe em seguida do ultimo ja feito
+*  Exemplo:
+   let nomeCliente;
+   nomeCliente = 'Luiz';
+   console.log(nomeCliente);
+   nomeCliente = 'Otávio';
+   console.log(nomeCliente); (funciona)
+*/
+
+// Observação 7
+
+// NÃO UTILIZE VAR, UTILIZE LET.
