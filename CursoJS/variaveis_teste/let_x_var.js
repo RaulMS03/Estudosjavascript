d84a8b4f3b47f7884437229d1bccacd148d59f65
Raul Mascarenhas <raulsmascarenhas@gmail.com>
2023-01-03 22:04:49 -0300

    Estudo atualizado

---
+++ CursoJS/variaveis_teste/let_x_var.js
@@ -0,0 +1,17 @@
+// Teste 01
+
+var nome = 'Luiz';
+var nome = 'Otávio';
+
+console.log(nome); // É possivel redeclarar variaveis com var.
+
+// Testa 02
+
+nome = 'Luiz'; // mesmo não declarando como var, o js entende como var (Nunca faça isso).
+console.log(nome); 
+
+/*
+    Nunca faça isso, não crie variaveis sem const ou sem let (REGRA).
+    Isso ira influenciar no andamento do seu programa.
+    E lembrando, NÃO USE VAR.
+*/
